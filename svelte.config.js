a5ac134763a5efbb0191b1283f6f9f38f5ab4640
Joel Perca <joelperca.vc@gmail.com>
2025-03-20 12:30:06 -0300

    ci: 🎡 config for github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,21 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		}),
 		paths: {
-      base: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
-    }
+			base: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
+		},
+		prerender: {
+			default: true,
+			handleHttpError: ({ path, referrer, message }) => {
+				console.warn(`${path} referido desde ${referrer} falló en la prerenderización: ${message}`);
+			}
+		}
 	}
 };
 
