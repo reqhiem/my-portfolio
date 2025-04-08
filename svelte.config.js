a9eee690882fe02297764bbe1fd7a0a1f093db3d
Joel Perca <joelperca.vc@gmail.com>
2025-04-08 12:56:45 -0300

    fix: 🐛 svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,11 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}),
+		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
-		}
-	}
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+	},
 };
 
 export default config;
